5ed1313369e136b0c9f0ae1ba873eb10644dfb4b
Ferivonus <ferivonus@hotmail.com>
2025-09-23 23:35:23 +0300

    first commit

--- Mustang-horse/Mustang-horse.cpp
+++ Mustang-horse/Mustang-horse.cpp
@@ -1,20 +1,44 @@
-// Mustang-horse.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+// Bu fonksiyon, terminale karakterleri tek tek yazdýrýr
+void typeWriter(const std::string& text, int delay_ms) {
+    for (char c : text) {
+        std::cout << c << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Yeþil renk
+    std::cout << "\033[32m";
+    std::cout << "                  /\\_" << std::endl;
+    std::cout << "  .-.   _.-''-.  \\  '" << std::endl;
+    std::cout << "  `  `''  .    `'  ) " << std::endl;
+    std::cout << "   \\   `'-.   ._,  (" << std::endl;
+    std::cout << "    `'-._  `''--'  `'._" << std::endl;
+
+    // Rengi sýfýrla
+    std::cout << "\033[0m";
+
+    std::cout << "\n";
+
+    // Sarý renk
+    std::cout << "\033[33m";
+    typeWriter("Mustang-horse projesi baþlýyor...", 50);
+    std::cout << "\n";
+
+    // Cam göbeði rengi
+    std::cout << "\033[36m";
+    typeWriter("Kodlar indirildi ve çalýþtýrýlýyor!", 50);
+    std::cout << "\n";
+
+    // Yeþil renk
+    std::cout << "\033[32m";
+    typeWriter("Baþarýlý! Program hazýr ve çalýþýyor.", 50);
+    std::cout << "\033[0m\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
